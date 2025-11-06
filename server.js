6394130f21d9066ebd41ea4a22d755067e50576c
Muazzam Younus - RI <Muazzam Younus - RI>
2025-11-06 11:00:04 +0500

    Update

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv/config';
 import { errorHandler } from './middlewares/errorHandler.js';
 import playerRoutes from './routes/playerRoutes.js';
 import teamRoutes from './routes/teamRoutes.js';
 import matchRoutes from './routes/matchRoutes.js';
 import statisticsRoutes from './routes/statisticsRoutes.js';
 
-dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 // Middlewares
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  origin: process.env.CORS_ORIGIN,
   credentials: true
 }));
 app.use(express.json());
